4735025f0df3d0e37ce800559b2a7599c5235c65
Max <bogachov_m@mail.ru>
2021-11-22 23:46:55 +0200

    initial commit

---
+++ lab6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+    /*
+    int min, max;
+    float x, h, y, ab;
+    cout << "enter min, max, h and ab" << endl;
+    cin >> min >> max >> h >> ab;
+    x = min;
+    while (x <= max + h / 2) {
+        y = ab + sin(x * x) - x * 0.5;
+        cout << setw(7) << x << setw(7) << y << endl;
+        x++;
+    }
+    */
+    int min, max;
+    float x, h, y, ab;
+    cout << "enter min, max, h and ab" << endl;
+    cin >> min >> max >> h >> ab;
+    x = min;
+    do {
+        y = ab + sin(x * x) - x * 0.5;
+        cout << setw(7) << x << setw(7) << y << endl;
+        x++;
+    } while (x <= max + h / 2);
+}
+
