0cebdb1a524295f560db88cd178206666ae46eb7
Max <bogachov_m@mail.ru>
2021-11-22 23:58:49 +0200

    deleted some parts of code

--- lab6.cpp
+++ lab6.cpp
@@ -4,18 +4,6 @@
 using namespace std;
 int main()
 {
-    /*
-    int min, max;
-    float x, h, y, ab;
-    cout << "enter min, max, h and ab" << endl;
-    cin >> min >> max >> h >> ab;
-    x = min;
-    while (x <= max + h / 2) {
-        y = ab + sin(x * x) - x * 0.5;
-        cout << setw(7) << x << setw(7) << y << endl;
-        x++;
-    }
-    */
     int min, max;
     float x, h, y, ab;
     cout << "enter min, max, h and ab" << endl;
